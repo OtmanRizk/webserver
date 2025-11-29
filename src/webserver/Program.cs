88bf7314ded26e402b47bbe5dd39f945f8cc2a4d
OtmanRizk <rizkotman@gmail.com>
2025-11-29 15:47:49 +0100

    ref(): gono to c#

---
+++ src/webserver/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+TcpListener myListener;
+int port = 5050;
+IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+//web server path
+string WebServerPath = @"client";
+
+
+myListener = new TcpListener(localAddr, port);
+myListener.Start();
+Console.WriteLine($"Web Server Running on {localAddr.ToString()} on port {port}... Press ^C to Stop...");
+Thread th = new Thread(new ThreadStart(StartListen));
+th.Start();
+
+void StartListen()
+{
+        while (true)
+        {
+                try
+                {
+                        TcpClient client = myListener.AcceptTcpClient();
+                        NetworkStream stream = client.GetStream();
+                        if (stream.CanRead)
+                                continue;
+
+                        byte[] readBuffer = new byte[1024];
+                        StringBuilder completeMesssage = new StringBuilder();
+                        int numberBytesRead = stream.Read(readBuffer, 0, readBuffer.Length);
+
+                        completeMesssage.Append(Encoding.UTF8.GetString(readBuffer, 0, numberBytesRead));
+
+
+
+                        Console.WriteLine($"Message received: {completeMesssage}");
+                        client.Close();
+                }
+                catch (SocketException)
+                {
+                        Console.WriteLine("SocketException");
+
+                }
+                catch (System.Exception err)
+                {
+                        Console.WriteLine($"Message: {err.Message}\n StackTrace: {err.StackTrace}");
+
+                }
+
+        }
+}
+
+void handleRequest()
+{
+
+}
+
+
